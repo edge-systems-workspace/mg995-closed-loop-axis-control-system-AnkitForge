905280d20db9968e787001f251eafd27bbef1fad
AnkitForge <ankit.patel_cs.aiml24@gla.ac.in>
2026-02-22 23:35:26 +0530

    rotate servo

--- src/main.cpp
+++ src/main.cpp
@@ -42,3 +42,12 @@ Servo myServo;
 void setup() {
     myServo.attach(9);
 }
+void loop() {
+    myServo.write(0);
+    delay(1000);
+    myServo.write(90);
+    delay(1000);
+    myServo.write(180);
+    delay(1000);
+
+}
